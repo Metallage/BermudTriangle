70b15dbcb1d3e87f354f5ddb076ea19a090b40b0
Egor Timashev <metallagetemp@gmail.com>
2018-11-25 16:36:14 +0300

    тест

---
+++ MathLogik/BermudTriangleLogic.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLogik
+{
+    public class BermudTriangleLogic
+    {
+        private int[] simField = new int[2];
+        private int[,] triangleField = new int[3,2];
+        private static Random xRandom;
+        private static Random yRandom;
+
+        public BermudTriangleLogic(int xCoord, int yCoord)
+        {
+            simField[0] = xCoord;
+            simField[1] = yCoord;
+            xRandom = new Random(xCoord);
+            yRandom = new Random(yCoord);
+        }
+      
+    }
+}
---
+++ MathLogik/Triangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLogik
+{
+    class Triangle
+    {
+        private int[,] triDots = new int[3, 2];
+        private decimal[] triLines = new decimal[3];
+
+
+
+        private decimal calcTriSquare()
+        {
+            decimal square = 0;
+
+            return square;
+        }
+    }
+}
